f752514542eb850f3945ffc8aed654a39108f29b
Joey Goksu <me@yusufgoksu.com>
2021-05-08 22:21:52 +0300

    moved jest related files

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ const { defaults: tsjPreset } = require('ts-jest/presets');
 module.exports = {
   ...tsjPreset,
   preset: 'react-native',
-  setupFiles: ['./jestSetupFile.js'],
+  setupFiles: ['./jest/jestSetupFile.js'],
   transform: {
     ...tsjPreset.transform,
     '\\.js$': '<rootDir>/node_modules/react-native/jest/preprocessor.js',
@@ -19,8 +19,8 @@ module.exports = {
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/assetsTransformer.js',
-    '\\.(css|less)$': '<rootDir>/assetsTransformer.js',
+      '<rootDir>/jest/assetsTransformer.js',
+    '\\.(css|less)$': '<rootDir>/jest/assetsTransformer.js',
   },
   // This is the only part which you can keep
   // from the above linked tutorial's config:
