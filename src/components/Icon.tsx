cc7e363746f653ac614796cd38ec08ce2bd824b8
Joey Goksu <me@yusufgoksu.com>
2021-05-08 22:13:15 +0300

    Delete Icon.tsx

--- src/components/Icon.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { Icon } from '@ui-kitten/components';
-
-export const CustomIcon: React.FC<{ name: string }> = props => (
-  <Icon {...props} name={props.name} />
-);
